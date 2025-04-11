c2cee4e0465ea1a8dd25adccd99781f093d32c71
unknown <ybilqis76@gmail.com>
2025-04-11 22:29:01 +0200

    Completed Task1 method

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,15 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public static int Task1(List<Integer> a, List<Integer> b) {
+        int sum = 0;
+        for (int index : b) {
+            if (index >= 0 && index < a.size()) {
+                sum += a.get(index);
+            }
+        }
+        return sum;
+    }
+
 }
